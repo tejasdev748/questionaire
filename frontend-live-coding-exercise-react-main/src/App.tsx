7103d6fb2a27f59d619931df2c86c0ad1976cacc
tejas <tejasdev749@gmail.com>
2024-07-30 14:19:33 +0530

    Support to select answers

---
+++ frontend-live-coding-exercise-react-main/src/App.tsx
@@ -0,0 +1,74 @@
+import React, { Component, ReactElement } from "react";
+import { QUESTIONS } from "./questions";
+
+type ANSWER = "yes" | "no";
+
+class App extends Component {
+  state = {
+    answerSet: new Map<number, ANSWER>(),
+  };
+
+  showQuestions = () => {
+    let questionsView: ReactElement[] = [];
+    for (const questionNo in QUESTIONS) {
+      if (Object.hasOwnProperty.call(QUESTIONS, questionNo)) {
+        const question = (
+          <li>
+            <div>{QUESTIONS[questionNo]}</div>
+            <div>{this.choooseAnswer(questionNo)}</div>
+          </li>
+        );
+
+        questionsView.push(question);
+      }
+    }
+    return <ol>{questionsView}</ol>;
+  };
+
+  choooseAnswer = (questionNo: string) => {
+    const selectedAnswer = this.state.answerSet.has(Number(questionNo))
+      ? this.state.answerSet.get(Number(questionNo))
+      : undefined;
+    return (
+      <>
+        <div>
+          <input
+            type="radio"
+            checked={selectedAnswer === "yes"}
+            onChange={() => this.onSelectAnswer(Number(questionNo), "yes")}
+          />
+          <label>Yes</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            checked={selectedAnswer === "no"}
+            onChange={() => this.onSelectAnswer(Number(questionNo), "no")}
+          />
+          <label>No</label>
+        </div>
+      </>
+    );
+  };
+
+  onSelectAnswer = (questionNo: number, answer: "yes" | "no") => {
+    const answerSet = new Map(this.state.answerSet);
+    answerSet.set(questionNo, answer);
+    this.setState({
+      answerSet,
+    });
+  };
+
+  render() {
+    return (
+      <div className="main__wrap">
+        <main className="container">
+          <div>TODO</div>
+          {this.showQuestions()}
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
